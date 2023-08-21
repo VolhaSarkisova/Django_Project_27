4ffd48ce09309866d7c8c64d285b02c2aab5a872
VolhaSarkisova <olkavalkiria@gmial.com>
2023-08-21 16:29:53 +0300

    -fix: add description for goods and categories, add img, price, is_active for goods

--- goods/models.py
+++ goods/models.py
@@ -5,10 +5,17 @@ class categories(models.Model):
     name = models.CharField(max_length=70,
                             verbose_name="Сategory",
                             help_text="Enter a сategory title")
+    description = models.TextField(max_length=3000,
+                                   verbose_name="Сategory description",
+                                   help_text="Must contain only 3000 characters")
     img = models.BinaryField()
     is_active = models.BooleanField(null=True)
     def __str__(self):
         return {self.name}
+    class Meta:
+        verbose_name = 'Сategory'
+        verbose_name_plural = 'Categories'
+        ordering = ['-is_active', 'name']
 class goods(models.Model):
     name = models.CharField(max_length=100,
                             verbose_name="Good",
@@ -16,5 +23,15 @@ class goods(models.Model):
     categories = models.ForeignKey(categories,
                                    on_delete=models.PROTECT,
                                    limit_choices_to={"is_active": True})
+    description = models.TextField(max_length=3000,
+                                   verbose_name="Good description",
+                                   help_text="Must contain only 3000 characters")
+    img = models.BinaryField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_active = models.BooleanField(null=True)
     def __str__(self):
         return f'{self.categories.name}: {self.name}'
+    class Meta:
+        verbose_name = 'Good'
+        verbose_name_plural = 'Goods'
+        ordering = ['-is_active', 'name']
