decd45576adf538e6190a6e24bbbdf15ff56eb79
VolhaSarkisova <olkavalkiria@gmial.com>
2023-08-22 14:05:04 +0300

    -fix: add field in list_display Goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import Categories, Goods
 # Register your models here.
+
+
+def capitalize_title(modeladmin, request, queryset):
+    for model in queryset:
+        model.name = model.name.capitalize()
+        model.save()
+
+capitalize_title.short_description = 'Нормализовать заголовок'
+
+@admin.register(Goods)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'is_active')
+    list_filter = ('is_active', )
+    search_fields = ('name', )
+    actions = [capitalize_title]
+
+@admin.register(Categories)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+    list_filter = ('is_active', )
+    search_fields = ('name', )
+    actions = [capitalize_title]
